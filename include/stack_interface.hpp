319c2ffecba7ea3ae9c8a83d303884718fadf2c2
Lari <lara.a.torletti@gmail.com>
2024-04-25 21:16:48 -0300

    stack_interface con virtual implementado

--- include/stack_interface.hpp
+++ include/stack_interface.hpp
@@ -1,14 +1,26 @@
+/**
+ * @file stack_interface.hpp
+ * @author Pojmaevich Mirko (mirkopoj@gmail.com)
+ *         Torletti Lara (lara.a.torletti@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #pragma once
 #include <cstddef>
 
 class StackableObject {
 public:
-  void print();
+  virtual void print() = 0;
 };
 
 class Stack {
 public:
-  bool push(StackableObject*);
-  StackableObject* pop();
-  size_t get_count();
+  virtual bool push(StackableObject*) = 0;
+  virtual StackableObject* pop() = 0;
+  virtual size_t get_count() = 0;
 };
