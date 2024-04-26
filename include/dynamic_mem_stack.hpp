15903ef1ad3fe26cccf30e6787dd0ed31ec75c4a
Mirko <mirkopoj@hotmail.com>
2024-04-26 00:12:25 -0300

    Implementado es stack dinamico

--- include/dynamic_mem_stack.hpp
+++ include/dynamic_mem_stack.hpp
@@ -7,18 +7,20 @@
 
 class DynamicMemStack : public Stack {
 public:
-	DynamicMemStack();
-	DynamicMemStack(size_t);
-	DynamicMemStack(DynamicMemStack &&) = delete;
-	DynamicMemStack(const DynamicMemStack &) = delete;
-	DynamicMemStack &operator=(DynamicMemStack &&) = delete;
-	DynamicMemStack &operator=(const DynamicMemStack &) = delete;
-	~DynamicMemStack();
+  DynamicMemStack();
+  DynamicMemStack(size_t);
+  DynamicMemStack(DynamicMemStack &&) = delete;
+  DynamicMemStack(const DynamicMemStack &) = delete;
+  DynamicMemStack &operator=(DynamicMemStack &&) = delete;
+  DynamicMemStack &operator=(const DynamicMemStack &) = delete;
+  ~DynamicMemStack();
 
-	virtual bool push(const StackableObject*) override;
-	virtual StackableObject* pop() override;
-	virtual size_t get_count() const override;
+  virtual bool push(StackableObject *const) override;
+  virtual StackableObject *pop() override;
+  virtual size_t get_count() const override;
 
-	StackableObject** stack;
-	size_t count;
+private:
+  StackableObject **stack;
+  size_t count;
+  size_t capacity;
 };
--- src/dynamic_mem_stack.cpp
+++ src/dynamic_mem_stack.cpp
@@ -1,15 +1,41 @@
 #include "../include/dynamic_mem_stack.hpp"
+#include <cstdlib>
 
 DynamicMemStack::DynamicMemStack()
-    : count(0), stack(new StackableObject* [DYNAMIC_STACK_INITIAL_CAPACITY]){};
+    : count(0), stack(new StackableObject *[DYNAMIC_STACK_INITIAL_CAPACITY]),
+      capacity(DYNAMIC_STACK_INITIAL_CAPACITY){};
 
-DynamicMemStack::DynamicMemStack(size_t initial_count)
-    : count(0), stack(new StackableObject* [initial_count]){};
+DynamicMemStack::DynamicMemStack(size_t initial_capacity)
+    : count(0), stack(new StackableObject *[initial_capacity]),
+      capacity(initial_capacity){};
 
-DynamicMemStack::~DynamicMemStack() {
-	delete[] stack;
-};
+DynamicMemStack::~DynamicMemStack() { delete[] stack; };
 
-bool DynamicMemStack::push(const StackableObject* new_value) {
-	return true;
+bool DynamicMemStack::push(StackableObject *const new_value) {
+  if (!new_value) {
+    return false;
+  }
+  if (count == capacity) {
+    size_t new_capacity = capacity * 2;
+    StackableObject **new_stack =
+        (StackableObject **)realloc(stack, new_capacity);
+    if (!new_stack) {
+      return false;
+    }
+    this->stack = new_stack;
+    this->capacity = new_capacity;
+  }
+  this->stack[this->count] = new_value;
+  return true;
 }
+
+StackableObject *DynamicMemStack::pop() {
+  StackableObject *ret = nullptr;
+  if (this->count > 0) {
+    this->count--;
+    ret = this->stack[this->count];
+  }
+  return ret;
+}
+
+size_t DynamicMemStack::get_count() const { return this->count; }
